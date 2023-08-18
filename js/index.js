0d95fe9fdf0b7fb6d0594782bba0bf660e835658
Salma Hussein <salmahussienhassan>
2023-08-19 02:21:20 +0300

    push

--- js/index.js
+++ js/index.js
@@ -1,7 +1,26 @@
+// scroll
+let secondSectionTop=$('.second').offset().top
+console.log(secondSectionTop)
+
 $('.scroll').click(function(){
+
     $('body,html').animate({scrollTop:'0px'},500)
  })
+$(window).scroll(function(){
+    console.log($(window).scrollTop()) 
+    if($(window).scrollTop()>secondSectionTop)
+    
+   {
+    $('.scroll').css('display','flex')
+   $('.open').addClass('open-scroll')
+}
+    else
+    {$('.scroll').css('display','none')
+    $('.open').removeClass('open-scroll')
+}
+})
 
+// aside
  $('.open').click(function(){
     $('nav').animate({left: '0'},500)
  })
@@ -9,3 +28,13 @@ $('.scroll').click(function(){
  $('.fa-xmark').click(function(){
     $('nav').animate({left: '-240px'},1000)
  })
+
+
+//  cordion
+$('.head').click(function() {
+    
+    $('.body').not($(this).next('.body')).slideUp(500);
+    // console.log(this)
+   
+    $(this).next('.body').slideToggle(500);
+  });
